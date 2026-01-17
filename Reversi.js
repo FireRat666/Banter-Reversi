0ecd54739f98446b0db9d21b14215ee132f2370a
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-01-17 16:01:39 +1100

    update Reversi.js Transparency First

--- Reversi.js
+++ Reversi.js
@@ -434,7 +434,7 @@
 
         const color = hexToVector4(colorHex, opacity);
 
-        const shader = config.lighting === 'lit' ? "Standard" : (opacity < 1.0 ? "Unlit/DiffuseTransparent" : "Unlit/Diffuse");
+        const shader = opacity < 1.0 ? "Unlit/DiffuseTransparent" : (config.lighting === 'lit' ? "Standard" : "Unlit/Diffuse");
         // Use cacheBust to create unique material instance for objects that need dynamic colors
         await obj.AddComponent(new BS.BanterMaterial(shader, "", color, BS.MaterialSide.Front, false, cacheBust || ""));
 
